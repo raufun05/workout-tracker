8f14738b7e141f82f618b563be6a5171ec20cdc7
Raufun <raufun05@yahoo.com>
2020-10-23 19:43:31 -0600

    added routes and models folders

--- seeders/seed.js
+++ seeders/seed.js
@@ -121,6 +121,17 @@ let workoutSeed = [
         sets: 4
       }
     ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-1),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench",
+        duration: 30,
+        distance: 2
+      }
+    ]
   }
 ];
 
